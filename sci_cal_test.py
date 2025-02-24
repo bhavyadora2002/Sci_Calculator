a1ffa6faebd6ce9cc91dcc41d18bb71972be15ae
bhavyadora2002 <bhavyadora2002@gmail.com>
2025-02-25 01:24:03 +0530

    update

--- sci_cal_test.py
+++ sci_cal_test.py
@@ -8,8 +8,8 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(square_root(-1), "Imaginary roots")
     def test_factorial(self):
         self.assertEqual(factorial(5), 120)
-        self.assertEqual(factorial(0), 1)
-        self.assertEqual(factorial(-1), "Enter positive number")
+        self.assertEqual(factorial(0), 0)
+        self.assertEqual(factorial(1), "Enter positive number")
 
 if __name__ == "__main__":
     unittest.main()
